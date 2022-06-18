7331f61885f717a75a714ab160c7f9ea220b497d
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-06-17 19:18:49 -0500

    Update rsrcdump/resconverters.py
    Co-authored-by: Iliyas Jorio <iliyas@jor.io>

--- rsrcdump/resconverters.py
+++ rsrcdump/resconverters.py
@@ -103,7 +103,7 @@ class CicnConverter(ResourceConverter):
         super().__init__(separate_file='.png')
 
     def convert(self, res: Resource,
-                res_map: Dict[bytes, Dict[int, Resource]]) -> bytes:
+                res_map: dict[bytes, dict[int, Resource]]) -> bytes:
         w, h, data = convert_cicn_to_image(res.data)
         return pack_png(data, w, h)
 
